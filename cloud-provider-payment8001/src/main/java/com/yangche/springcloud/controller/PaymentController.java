077e46a64d441a4772f9c4a9ae20e7a3972f7883
yangche <1351767847@qq.com>
2020-06-01 21:12:49 +0800

    actuator微服务信息完善

--- cloud-provider-payment8001/src/main/java/com/yangche/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/yangche/springcloud/controller/PaymentController.java
@@ -6,7 +6,12 @@ import com.yangche.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -17,6 +22,9 @@ public class PaymentController {
     //将配置文件中的server.port导入进来，只是为了查看调用的是哪个服务
     @Value("${server.port}")
     private String serverPort;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping("/create")
     public CommonResult create(@RequestBody Payment payment) {
@@ -38,6 +46,21 @@ public class PaymentController {
         }else{
             return new CommonResult(444, "查询数据失败");
         }
+    }
+
+    @GetMapping("/discovery")
+    public Object paymentDiscovery(){
+        List<String> services = discoveryClient.getServices();
+        //服务发现，获取所有的服务
+        services.forEach(element->{
+            log.info("*******element:{}",element);
+        });
+        //获取所有对应的实例
+        List<ServiceInstance> instances = discoveryClient.getInstances("cloud-payment-service");
+        instances.forEach(instance->{
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        });
+        return this.discoveryClient;
     }
 
 }
