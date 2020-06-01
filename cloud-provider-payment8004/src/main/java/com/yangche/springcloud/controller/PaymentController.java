3f4636186c2b1d176ec6fd5621e3fff4e83fb3fa
yangche <1351767847@qq.com>
2020-06-01 23:24:23 +0800

    zookeeper环境搭建好 新建zookeeper的payment模块，并且注册到zookeeper中，注册不用eureka用zookeeper

---
+++ cloud-provider-payment8004/src/main/java/com/yangche/springcloud/controller/PaymentController.java
@@ -0,0 +1,25 @@
+package com.yangche.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@Slf4j
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/zk")
+    public String connectedZk(){
+        String result = "serverPort:" + serverPort + "\tuuid:" + UUID.randomUUID();
+        log.info(result);
+        return result;
+    }
+}
